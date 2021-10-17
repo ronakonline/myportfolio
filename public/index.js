542aacc1d17b5e453c16001babe093a613c22dc5
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-10-17 12:03:09 +0530

    inti commit

---
+++ public/index.js
@@ -0,0 +1,70 @@
+var csstricks = {
+
+    init: function () {
+      csstricks.randomizeBackgrounds();
+    },
+  
+    generateRandomPoints: function (minSpread, maxSpread) {
+      let points = {};
+      points.a = `${getRandomInt(800, 1000)},${getRandomInt(minSpread, maxSpread)}`;
+      points.b = `${getRandomInt(600, 800)},${getRandomInt(minSpread, maxSpread)}`;
+      points.c = `${getRandomInt(400, 600)},${getRandomInt(minSpread, maxSpread)}`;
+      points.d = `${getRandomInt(200, 400)},${getRandomInt(minSpread, maxSpread)}`;
+      points.e = `${getRandomInt(0, 200)},${getRandomInt(minSpread, maxSpread)}`;
+      return points;
+    },
+  
+    randomizeHeader: function () {
+      let newPoints = csstricks.generateRandomPoints(120, 190);
+      let downFacingPoints = `M-4,-4 L1004,-4 L1004,100 L${newPoints.a} L${newPoints.b} L${newPoints.c} L${newPoints.d} L${newPoints.e} L-4,100 L-4,-4 Z`;
+      $("#jagged-top").attr("d", downFacingPoints);
+    },
+  
+    randomizeBackgrounds: function () {
+      csstricks.randomizeHeader();
+      requestInterval(function () {
+        csstricks.randomizeHeader();
+      }, 2000);
+    } };
+  
+  
+  
+  csstricks.init();
+  
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
+  function requestInterval(fn, delay) {
+    var requestAnimFrame = function () {
+      return window.requestAnimationFrame || function (callback, element) {
+        window.setTimeout(callback, 1000 / 60);
+      };
+    }(),
+    start = new Date().getTime(),
+    handle = {};
+    function loop() {
+      handle.value = requestAnimFrame(loop);
+      var current = new Date().getTime(),
+      delta = current - start;
+      if (delta >= delay) {
+        fn.call();
+        start = new Date().getTime();
+      }
+    }
+    handle.value = requestAnimFrame(loop);
+    return handle;
+  };
+
+  $(document).ready(function() {
+
+    var html = '';
+    for (var i = 1; i <= 50; i ++) {
+        html += '<div class="shape-container--'+i+' shape-animation"><div class="random-shape"></div></div>';
+    }
+      
+    document.querySelector('.shape').innerHTML += html;
+    
+    var $allShapes = $("[class*='shape-container--']");
+    
+    });
---
+++ src/components/header.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {Menu, MenuButton, MenuList,IconButton,} from "@chakra-ui/react";
+
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+export default function header() {
+  return (
+    <>
+      <div className="container-fluid head-container">
+        <nav className="navbar navbar-light">
+          <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">Ronak Pareek</span>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+              />
+              <MenuList></MenuList>
+            </Menu>
+          </div>
+        </nav>
+
+        <div className="row">
+          <div className="shape">
+            <div className="col-12 head-text">
+              <span>Hey! I am Ronak,</span>
+              <br />
+              <span>I build web related projects that</span>
+              <br />
+              <span>are useful to my clients.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <svg
+        viewBox="0 0 1000 200"
+        preserveAspectRatio="none"
+        className="site-header-background"
+      >
+        <defs>
+          <linearGradient id="header-gradient" x2="0%" y2="100%">
+            <stop offset="0%" stop-color="#0C0C0C" />
+            <stop offset="100%" stop-color="#0C0C0C" />
+          </linearGradient>
+        </defs>
+        <path
+          className="jagged-top"
+          id="jagged-top"
+          fill="url(#header-gradient)"
+          d="M-4,-4 L1004,-4 L1004,90 L804,90 L604,90 L404,90 L204,90 L-4,90 L-4,90 L-4,-4 Z"
+        />
+      </svg>
+    </>
+  );
+}
