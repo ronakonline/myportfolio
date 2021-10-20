8c5400c6bba33bfdd2405e89b9861971621e53ea
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-10-20 23:12:31 +0530

    fixed contact form responsive

--- src/components/contact.js
+++ src/components/contact.js
@@ -16,38 +16,35 @@ import {
   InputGroup,
   InputLeftElement,
   Textarea,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 import {
   MdPhone,
   MdEmail,
   MdLocationOn,
   MdFacebook,
   MdOutlineEmail,
-} from "react-icons/md";
-import { BsGithub, BsDiscord, BsPerson } from "react-icons/bs";
-import React from "react";
+} from 'react-icons/md';
+import { BsGithub, BsDiscord, BsPerson } from 'react-icons/bs';
 
 export default function contact() {
   return (
-    <Container
-      className="container-fluid skills-container"
-      bg="#9DC4FB"
-      maxW="full"
-      mt={0}
-      centerContent
-      overflow="hidden"
-    >
+    <Container bg="#9DC4FB" maxW="full" mt={0} centerContent overflow="hidden">
       <Flex>
-        <Box bg="#02054B" color="white" borderRadius="lg" m={[5, 5, 16]} p={[3,5,16]} maxW={["sm","md","full"]}>
+        <Box
+          bg="#02054B"
+          color="white"
+          borderRadius="lg"
+          m={[5, 5, 16]}
+          p={[3, 5, 16]}>
           <Box>
-            <Wrap spacing={[5, 5, 40]}>
+            <Wrap spacing={[10, 0, 20]}>
               <WrapItem>
                 <Box>
                   <Heading>Contact</Heading>
-                  <Text mt={[3,5,5]} color="gray.500">
-                    Fill up the form below to get in touch with me
+                  <Text mt={[3, 5, 5]} color="gray.500">
+                  Fill up the form below to get in touch with me
                   </Text>
-                  <Box mt={[0,0,10]} py={[5,8,10]}>
+                  <Box mt={[0, 0, 10]} py={[5, 8, 10]}>
                     <VStack pl={0} spacing={3} alignItems="flex-start">
                       <Button
                         size="md"
@@ -55,10 +52,9 @@ export default function contact() {
                         width="200px"
                         variant="ghost"
                         color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdPhone color="#1970F1" size="20px" />}
-                      >
-                        +91-9888888888
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdPhone color="#1970F1" size="20px" />}>
+                        +91-988888888
                       </Button>
                       <Button
                         size="md"
@@ -66,9 +62,8 @@ export default function contact() {
                         width="200px"
                         variant="ghost"
                         color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdEmail color="#1970F1" size="20px" />}
-                      >
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdEmail color="#1970F1" size="20px" />}>
                         hello@abc.com
                       </Button>
                       <Button
@@ -77,83 +72,85 @@ export default function contact() {
                         width="200px"
                         variant="ghost"
                         color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}
-                      >
-                        Bangalore, India
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
+                        Karnavati, India
                       </Button>
                     </VStack>
                   </Box>
-                  <HStack mt={[0,0,10]} spacing={5} px={5} alignItems="flex-start">
+                  <HStack
+                    mt={[0, 0, 10]}
+                    spacing={5}
+                    px={5}
+                    alignItems="flex-start">
                     <IconButton
                       aria-label="facebook"
                       variant="ghost"
                       size="lg"
-                      isRound="true"
-                      _hover={{ bg: "#0D74FF" }}
-                      iconSize="5rem"
+                      isRound={true}
+                      _hover={{ bg: '#0D74FF' }}
                       icon={<MdFacebook size="28px" />}
                     />
                     <IconButton
                       aria-label="github"
                       variant="ghost"
                       size="lg"
-                      isRound="true"
-                      _hover={{ bg: "#0D74FF" }}
-                      iconSize="5rem"
+                      isRound={true}
+                      _hover={{ bg: '#0D74FF' }}
                       icon={<BsGithub size="28px" />}
                     />
                     <IconButton
                       aria-label="discord"
                       variant="ghost"
                       size="lg"
-                      isRound="true"
-                      _hover={{ bg: "#0D74FF" }}
+                      isRound={true}
+                      _hover={{ bg: '#0D74FF' }}
                       icon={<BsDiscord size="28px" />}
                     />
                   </HStack>
                 </Box>
               </WrapItem>
-              <Box bg="white" borderRadius="lg" maxW="lg" width={350}>
-                <Box m={8} color="#0B0E3F">
-                  <VStack spacing={5}>
-                    <FormControl id="name">
-                      <FormLabel>Your Name</FormLabel>
-                      <InputGroup borderColor="#E0E1E7">
-                        <InputLeftElement
-                          pointerEvents="none"
-                          children={<BsPerson color="gray.800" />}
-                        />
-                        <Input type="text" size="md" />
-                      </InputGroup>
-                    </FormControl>
-                    <FormControl id="name">
-                      <FormLabel>Mail</FormLabel>
-                      <InputGroup borderColor="#E0E1E7">
-                        <InputLeftElement
-                          pointerEvents="none"
-                          children={<MdOutlineEmail color="gray.800" />}
-                        />
-                        <Input type="text" size="md" />
-                      </InputGroup>
-                    </FormControl>
-                    <FormControl id="name">
-                      <FormLabel>Message</FormLabel>
-                      <Textarea></Textarea>
-                    </FormControl>
-                    <FormControl id="name" float="right">
-                      <Button
-                        variant="solid"
-                        bg="#0D74FF"
-                        color="white"
-                        _hover={{}}
-                      >
-                        Send Message
-                      </Button>
-                    </FormControl>
-                  </VStack>
+              <WrapItem>
+                <Box bg="white" borderRadius="lg">
+                  <Box m={8} color="#0B0E3F">
+                    <VStack spacing={5}>
+                      <FormControl id="name">
+                        <FormLabel>Your Name</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement
+                            pointerEvents="none"
+                            children={<BsPerson color="gray.800" />}
+                          />
+                          <Input type="text" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>Mail</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement
+                            pointerEvents="none"
+                            children={<MdOutlineEmail color="gray.800" />}
+                          />
+                          <Input type="text" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>Message</FormLabel>
+                        <Textarea></Textarea>
+                      </FormControl>
+                      <FormControl id="name" float="right">
+                        <Button
+                          variant="solid"
+                          bg="#0D74FF"
+                          color="white"
+                          _hover={{}}>
+                          Send Message
+                        </Button>
+                      </FormControl>
+                    </VStack>
+                  </Box>
                 </Box>
-              </Box>
+              </WrapItem>
             </Wrap>
           </Box>
         </Box>
