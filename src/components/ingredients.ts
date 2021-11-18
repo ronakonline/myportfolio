2ccef93538861c5bf8164c81df605ac118cbb291
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-11-18 18:01:49 +0530

    skills div complete

--- src/components/ingredients.ts
+++ src/components/ingredients.ts
@@ -6,7 +6,22 @@ import tailwind from "../skills-images/tailwind.svg";
 import react from "../skills-images/react.png";
 import material from "../skills-images/material.svg";
 import javascript from "../skills-images/javascript.png";
-import jquery from "../skills-images/jquery.svg";
+import jquery from "../skills-images/jquery.png";
+import php from "../skills-images/php.png";
+import laravel from "../skills-images/laravel.png";
+import codeigniter from "../skills-images/codeigniter.png";
+import nodejs from "../skills-images/nodejs.png";
+import express from "../skills-images/express.png";
+import electron from "../skills-images/electron.png";
+import python from "../skills-images/python.png";
+import java from "../skills-images/java.png";
+import mongodb from "../skills-images/mongodb.png";
+import git from "../skills-images/git.png";
+import github from "../skills-images/github.png";
+import postgresql from "../skills-images/postgresql.png";
+import mysql from "../skills-images/mysql.png";
+import handlebars from "../skills-images/handlebars.png";
+import figma from "../skills-images/figma.png";
 export interface Ingredient {
   icon: any;
   label: string;
@@ -28,34 +43,32 @@ export const allIngredients = [
   },
   {
     icon: [
-      { img: html, title: "HTML" },
-      { img: css, title: "CSS" },
-      { img: bootstrap, title: "Bootstrap" },
-      { img: tailwind, title: "Tailwind" },
-      { img: react, title: "React" },
-      { img: material, title: "Material UI" },
+      { img: php, title: "PHP" },
+      { img: laravel, title: "Laravel" },
+      { img: codeigniter, title: "Codeigniter" },
+      { img: nodejs, title: "Nodejs" },
+      { img: express, title: "Express" },
+      { img: electron, title: "Electron" },
+      { img: python, title: "Python" },
+      { img: java, title: "Java" },
+      { img: handlebars, title: "Handlebars" },
+
     ],
     label: "Backend",
   },
   {
     icon: [
-      { img: html, title: "HTML" },
-      { img: css, title: "CSS" },
-      { img: bootstrap, title: "Bootstrap" },
-      { img: tailwind, title: "Tailwind" },
-      { img: react, title: "React" },
-      { img: material, title: "Material UI" },
+      { img: mongodb, title: "MongoDB" },
+      { img: mysql , title: "MySQL" },
+      { img: postgresql, title: "PostgreSQL" },
     ],
     label: "Database",
   },
   {
     icon: [
-      { img: html, title: "HTML" },
-      { img: css, title: "CSS" },
-      { img: bootstrap, title: "Bootstrap" },
-      { img: tailwind, title: "Tailwind" },
-      { img: react, title: "React" },
-      { img: material, title: "Material UI" },
+      { img: git, title: "Git" },
+      { img: github, title: "Github" },
+      { img: figma, title: "Figma" },
     ],
     label: "Tools & Others",
   }
--- src/components/skills.js
+++ src/components/skills.js
@@ -2,59 +2,63 @@ import "../framer.css";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { initialTabs as tabs } from "./ingredients";
-import { Grid,Box } from "@chakra-ui/layout";
+import { Grid, Box } from "@chakra-ui/layout";
+import { Tooltip } from "@chakra-ui/react";
 
 export default function App() {
-
   const [selectedTab, setSelectedTab] = useState(tabs[0]);
 
-
   return (
-    <div className="skills-container-main">
-      <div className="window">
-        <nav className="tab-nav">
-          <ul className="ul-list">
-            {tabs.map((item,index) => (
-              <li
-                key={item.label}
-                className={
-                  item === selectedTab ? "selected list-item" : "list-item"
-                }
-                onClick={() => setSelectedTab(item)}
-              >
-                {`${item.label}`}
-                {item === selectedTab ? (
-                  <motion.div className="underline" layoutId="underline" />
-                ) : null}
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <main className="main-container">
-          <AnimatePresence exitBeforeEnter>
-            <motion.div
-              key={selectedTab ? selectedTab.label : "empty"}
-              animate={{ opacity: 1, y: 0 }}
-              initial={{ opacity: 0, y: 20 }}
-              exit={{ opacity: 0, y: -20 }}
-              transition={{ duration: 0.15 }}
-            >
-              {/* {selectedTab ? selectedTab.icon : "😋"}
-              <span>{selectedTab.icon}</span> */}
-              <Grid templateColumns="repeat(5, 1fr)" gap={6} my={5}>
-              {selectedTab.icon.map((skill,index)=>(
-                
-                <Box key={index} mx={5}>
-                <img src={skill.img} alt="icon" height="64px" width="64px" />
-                {/* <p className="skilltitle">{skill.title}</p> */}
-                </Box>
-               
+    <>
+      <p className="skills-title">Skills</p>
+      <div className="skills-container-main">
+        <div className="window">
+          <nav className="tab-nav">
+            <ul className="ul-list">
+              {tabs.map((item, index) => (
+                <li
+                  key={item.label}
+                  className={
+                    item === selectedTab ? "selected list-item" : "list-item"
+                  }
+                  onClick={() => setSelectedTab(item)}
+                >
+                  {`${item.label}`}
+                  {item === selectedTab ? (
+                    <motion.div className="underline" layoutId="underline" />
+                  ) : null}
+                </li>
               ))}
-               </Grid>
-            </motion.div>
-          </AnimatePresence>
-        </main>
+            </ul>
+          </nav>
+          <main className="main-container">
+            <AnimatePresence exitBeforeEnter>
+              <motion.div
+                key={selectedTab ? selectedTab.label : "empty"}
+                animate={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: 20 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.15 }}
+              >
+                <Grid templateColumns="repeat(5, 1fr)" gap={6} my={5}>
+                  {selectedTab.icon.map((skill, index) => (
+                    <Box key={index} mx={5}>
+                      <Tooltip label={skill.title} placement="right-end">
+                        <img
+                          src={skill.img}
+                          alt="icon"
+                          height="64px"
+                          width="64px"
+                        />
+                      </Tooltip>
+                    </Box>
+                  ))}
+                </Grid>
+              </motion.div>
+            </AnimatePresence>
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
