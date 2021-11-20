7ef565079945a08e29b7d5d6044bcf5a979f8112
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-11-20 23:55:44 +0530

    projects added

--- src/components/projects.js
+++ src/components/projects.js
@@ -13,6 +13,18 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import Tilt from "react-parallax-tilt";
+import travelcrunch from "../projects-images/travelcrunch.png";
+import allthingsurban from "../projects-images/dabd.png";
+import zugad from "../projects-images/zugad.png";
+import match2marry from "../projects-images/match2marry.png";
+import borntogive from "../projects-images/borntogive.png";
+import brightsalon from "../projects-images/brightsalon.png";
+import burger from "../projects-images/burger.gif";
+import cprnow from "../projects-images/cprnow.png";
+import driveschool from "../projects-images/driveschool.png";
+import ptbright from "../projects-images/ptbright.png";
+import qncenter from "../projects-images/qncenter.png";
+import studeyabroad from "../projects-images/studeyabroad.gif";
 
 function filterProjects(projects, filter) {
   if (filter === "all") {
@@ -22,35 +34,79 @@ function filterProjects(projects, filter) {
 }
 
 export default function Projects() {
-  const [filter, setFilter] = useState("all");
+  const [filter, setFilter] = useState("CODEIGNITER");
   const projects = [
     {
-      name: "Project 1",
-      types: ["web", "mobile"],
+      name: "Travel Crunch",
+      types: ["PHP", "MYSQL", "CODEIGNITER"],
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image: "https://source.unsplash.com/random",
+        "Travel trips booking website where user can book different types of trips as per their requirment.Fully dynamic website all contents can be managed by admin panel.",
+      image: travelcrunch,
+      tags : ["PHP", "MYSQL",  "CMS", "HTML", "CSS", "CODEIGNITER", "BOOTSTRAP"],
     },
     {
-      name: "Project 2",
-      types: ["web", "codeigniter"],
+      name: "Zugad",
+      types: ["PHP", "MYSQL", "CODEIGNITER"],
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image: "https://source.unsplash.com/random",
+        "Service providing site B2B and B2C. Service seller can register and provide service to customer from zugad platform.Fully dynamic website with admin panel",
+      image: zugad,
+      tags : ["PHP", "MYSQL",  "CMS", "HTML", "CSS", "CODEIGNITER", "BOOTSTRAP"],
     },
     {
-      name: "Project 3",
-      types: ["web", "mobile"],
+      name: "DABD",
+      types: ["PHP", "MYSQL", "CODEIGNITER"],
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image: "https://source.unsplash.com/random",
+        "Dallas texas business listing website. Customer can find business and contact them with DABD platform. Business can list there business on DABD ",
+      image: allthingsurban,
+      tags : ["PHP", "MYSQL",  "CMS", "HTML", "CSS", "CODEIGNITER", "BOOTSTRAP"],
     },
     {
-      name: "Project 4",
-      types: ["web", "mobile"],
+      name: "Match 2 Marry",
+      types: ["PHP", "MYSQL", "CODEIGNITER"],
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image: "https://source.unsplash.com/random",
+        "Matrimony and Marriage related services providing site.Where user can find there partner or services required for marriage like DJ, Photographer, Food, etc. serice providers can register and offer there services ",
+      image: match2marry,
+      tags : ["PHP", "MYSQL",  "CMS", "HTML", "CSS", "CODEIGNITER", "BOOTSTRAP"],
+    },
+    // {
+    //   name: "Manhatan Burger",
+    //   types: ["PHP", "MYSQL", "CODEIGNITER"],
+    //   description:
+    //     "Food Store Website managable thew admin panel",
+    //   image: burger,
+    //   tags : ["PHP", "MYSQL",  "CMS", "HTML", "CSS", "CODEIGNITER", "BOOTSTRAP"],
+    // },
+    {
+      name: "PT Bright",
+      types: ["PHP", "MYSQL", "CODEIGNITER"],
+      description:
+        "Ecommerce Website to sell organic products ",
+      image: ptbright,
+      tags : ["PHP", "MYSQL",  "CMS", "HTML", "CSS", "CODEIGNITER", "BOOTSTRAP", "ECOMMERCE"],
+    },
+    {
+      name: "QN Center",
+      types: ["PHP", "MYSQL", "CODEIGNITER"],
+      description:
+        "QN Center (Quick Notice) where institues can register and post notes, blogs, important notices and student can access.",
+      image: qncenter,
+      tags : ["PHP", "MYSQL",  "CMS", "HTML", "CSS", "CODEIGNITER", "BOOTSTRAP"],
+    },
+    {
+      name: "CPRNOW",
+      types: ["PHP", "MYSQL", "CODEIGNITER"],
+      description:
+        "Institue that provides certification courses for cpr process",
+      image: cprnow,
+      tags : ["PHP", "MYSQL",  "CMS", "HTML", "CSS", "CODEIGNITER", "BOOTSTRAP"],
+    },
+    {
+      name: "Born to Give",
+      types: ["PHP", "MYSQL", "CODEIGNITER"],
+      description:
+        "Charity website where you can by lottery tickets and all the money of lottery tickets goes to charity",
+      image: borntogive,
+      tags : ["PHP", "MYSQL",  "CMS", "HTML", "CSS", "CODEIGNITER", "BOOTSTRAP"],
     },
   ];
 
@@ -88,7 +144,7 @@ export default function Projects() {
         Here are some of my projects.
       </Text>
       <HStack wrap="nowrap" justify="center" mt={{ base:8,md:0,lg:0 }}>
-        <Tag
+        {/* <Tag
           colorScheme="teal"
           borderRadius="full"
           size="lg"
@@ -98,7 +154,7 @@ export default function Projects() {
           cursor="pointer"
         >
           <TagLabel fontWeight="semibold">All</TagLabel>
-        </Tag>
+        </Tag> */}
         {projectTypes.map((type) => (
           <Tag
             key={type}
@@ -109,6 +165,7 @@ export default function Projects() {
             variant={filter === type ? "solid" : "outline"}
             onClick={() => setFilter(type)}
             cursor="pointer"
+            fontWeight="semibold"
           >
             {type}
           </Tag>
@@ -132,7 +189,7 @@ export default function Projects() {
               alt={project.name}
               width="100%"
               height={64}
-              objectFit="cover"
+              objectFit="fill"
               borderRadius="lg"
               shadow="lg"
             />
@@ -140,13 +197,19 @@ export default function Projects() {
             <Heading as="h3" size="lg" mt={4} color="teal.600" >
               {project.name}
             </Heading>
+            <Box minH={28}>
             <Text fontSize="md" mt={2}color="gray.600">
               {project.description}
             </Text>
-            <Stack isInline spacing={4} my={4}>
-              {project.types.map((type) => (
+            </Box>
+            <HStack isInline spacing={4} my={4} wrap="wrap" justifyContent="start" mx={2}>
+              {project.tags.map((type) => (
                 <Tag
-
+                  style={{
+                    marginBottom: "16px",
+                    marginLeft:"4px",
+                    marginRight:"4px",
+                  }}
                   key={type}
                   colorScheme="teal"
                   borderRadius="full"
@@ -154,11 +217,12 @@ export default function Projects() {
                   px={{ base: 4, md: 4 }}
                   variant="solid"
                   shadow="md"
+                 
                 >
                   {type}
                 </Tag>
               ))}
-            </Stack>
+            </HStack>
           </Box>
         ))}
       </SimpleGrid>
